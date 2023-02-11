d0a8f0629bdac7d958e10479a505de201142e46d
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-11 22:25:46 +0100

    feat: add pusher/ trigger notification

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: "eu",
+  useTLS: true
+});
+
+pusher.trigger("my-channel", "my-event", {
+  title: 'Alert',
+  message: 'Success show notification in your device.',
+});
