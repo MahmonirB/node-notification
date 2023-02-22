197f4b76fe36fb09e0a189a5fecf37d5b09838ed
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-23 00:04:20 +0100

    feat: Add session and session config

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ const config = require("../config");
 
 router.get("/", (req, res) => {
   res.redirect(
-    `https://anilist.co/api/v2/oauth/authorize?client_id=${config.CLIENT_ID}&redirect_uri=${config.REDIRECT_URL}&response_type=code`
+    `https://anilist.co/api/v2/oauth/authorize?client_id=${config.CLIENT_ID}&redirect_uri=${config.REDIRECT_URI}&response_type=code`
   );
 });
 
