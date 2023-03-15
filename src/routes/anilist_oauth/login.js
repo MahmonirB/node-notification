4ae3e088e63063927ffe36ce968b42714e631ecb
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-03-15 23:31:39 +0100

    fix: set redirect url as constant

--- src/routes/anilist_oauth/login.js
+++ src/routes/anilist_oauth/login.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const config = require("../../../config");
 
+const REDIRECT_URL = `https://anilist.co/api/v2/oauth/authorize?client_id=${config.ANILIST_CLIENT_ID}&redirect_uri=${config.ANILIST_REDIRECT_URL}&response_type=code`;
+
 router.get("/", (req, res) => {
-  res.redirect(
-    `https://anilist.co/api/v2/oauth/authorize?client_id=${config.ANILIST_CLIENT_ID}&redirect_uri=${config.ANILIST_REDIRECT_URL}&response_type=code`
-  );
+  res.redirect(REDIRECT_URL);
 });
 
 module.exports = router;
